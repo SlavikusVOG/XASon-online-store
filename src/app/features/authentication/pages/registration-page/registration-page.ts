13cdbd114729fb6f2e398180c9c2ceaf2a18f3c1
SlavikusVOG <slavaolish@gmail.com>
2026-05-16 16:26:41 +0300

    feat: generate component

---
+++ src/app/features/authentication/pages/registration-page/registration-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xas-registration-page',
+  imports: [],
+  templateUrl: './registration-page.html',
+  styleUrl: './registration-page.scss',
+})
+export class RegistrationPage {}
