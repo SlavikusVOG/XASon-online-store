11639099d25992ac0da3cf3b518899bae165b461
xevoider <xevoider@gmail.com>
2026-05-16 18:29:52 +0300

    feat: create catalog page, mock data and product card view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Catalog } from './features/catalog/pages/catalog/catalog';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'catalog',
+    component: Catalog,
+  },
+];
---
+++ src/app/types/features/catalog/product.types.ts
@@ -0,0 +1,8 @@
+// TODO моковый тип, актуализировать когда завяжемся на данные с коммерстулзов
+export type Product = {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  isInCart: boolean;
+};
