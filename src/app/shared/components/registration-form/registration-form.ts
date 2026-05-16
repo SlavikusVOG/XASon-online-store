3d3453fa44e5e70220b610eda35d51afc037791b
SlavikusVOG <slavaolish@gmail.com>
2026-05-16 16:27:03 +0300

    feat: generate registration form component

---
+++ src/app/shared/components/registration-form/registration-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xas-registration-form',
+  imports: [],
+  templateUrl: './registration-form.html',
+  styleUrl: './registration-form.scss',
+})
+export class RegistrationForm {}
