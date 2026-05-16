ed33ca3bbe6856106481e7f9b2ab266038e9494c
xevoider <xevoider@gmail.com>
2026-05-16 19:34:29 +0300

    refactor: use better type

--- src/app/shared/pipes/yes-no.pipe.ts
+++ src/app/shared/pipes/yes-no.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'yesNo',
 })
 export class YesNoPipe implements PipeTransform {
-  transform(value: boolean): unknown {
+  transform(value: boolean): 'Yes' | 'No' {
     return value ? 'Yes' : 'No';
   }
 }
