8187317cea52231d858140a503bde1750c003158
SlavikusVOG <slavaolish@gmail.com>
2026-05-16 16:10:06 +0300

    feat: update form input component with password visibility toggle

--- src/app/shared/components/form-input/form-input.ts
+++ src/app/shared/components/form-input/form-input.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal, WritableSignal, OnInit } from '@angular/core';
 import { Field, FormField } from '@angular/forms/signals';
 
 @Component({
@@ -7,11 +7,19 @@ import { Field, FormField } from '@angular/forms/signals';
   templateUrl: './form-input.html',
   styleUrl: './form-input.scss',
 })
-export class FormInput {
-  label = input.required<string>();
-  type = input<string>('text');
-  placeholder = input<string>('');
+export class FormInput implements OnInit {
+  inputLabel = input.required<string>();
+  incomingType = input.required<string>();
+  inputPlaceholder = input<string>('');
   errorMessage = input<string>('');
-  id = input.required<string>();
+  inputId = input.required<string>();
   formField = input.required<Field<string>>();
+  inputType: WritableSignal<string> = signal('');
+  togglePasswordVisibility(): void {
+    this.inputType.set(this.inputType() === 'password' ? 'text' : 'password');
+  }
+
+  ngOnInit(): void {
+    this.inputType.set(this.incomingType());
+  }
 }
