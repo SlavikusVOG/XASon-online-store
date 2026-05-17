a18cb42c289f25f80c2355994748f5d424d0e17b
Anna Galkevich <emikot321@gmail.com>
2026-05-17 23:20:22 +0300

    feat: implement layout-based routing with AuthLayout and MainLayout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './core/layouts/auth-layout/auth-layout';
+import { MainLayout } from './core/layouts/main-layout/main-layout';
 import { LoginPage } from './features/authentication/pages/login-page/login-page';
 import { Catalog } from './features/catalog/pages/catalog/catalog';
 
 export const routes: Routes = [
   {
-    path: 'login',
-    component: LoginPage,
+    path: '',
+    component: AuthLayout,
+    children: [{ path: 'login', component: LoginPage }],
   },
   {
-    path: 'catalog',
-    component: Catalog,
+    path: '',
+    component: MainLayout,
+    children: [{ path: 'catalog', component: Catalog }],
   },
 ];
