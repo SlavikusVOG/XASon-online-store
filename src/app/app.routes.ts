73a8bd1dfdaf3319bfc468984724a61c7bb01ac9
SlavikusVOG <slavaolish@gmail.com>
2026-05-17 18:00:49 +0300

    feat: add registration route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './features/authentication/pages/login-page/login-page';
+import { RegistrationPage } from '../app/features/authentication/pages/registration-page/registration-page';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginPage,
+  },
+  {
+    path: 'register',
+    component: RegistrationPage,
   },
 ];
