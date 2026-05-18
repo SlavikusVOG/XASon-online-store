dffa781fd05cbfd56bbc5626e335ccf37bb8031e
SlavikusVOG <slavaolish@gmail.com>
2026-05-18 17:51:45 +0300

    feat: add basket and basket-item components

---
+++ src/app/features/cart/components/basket-item/basket-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xas-basket-item',
+  imports: [],
+  templateUrl: './basket-item.html',
+  styleUrl: './basket-item.scss',
+})
+export class BasketItem {}
