592949d1764e30a3cf693618a83d381fb9a57426
SlavikusVOG <slavaolish@gmail.com>
2026-05-18 14:49:32 +0300

    feat: add cart page component

---
+++ src/app/features/cart/pages/cart-page/cart-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xas-cart-page',
+  imports: [],
+  templateUrl: './cart-page.html',
+  styleUrl: './cart-page.scss',
+})
+export class CartPage {}
