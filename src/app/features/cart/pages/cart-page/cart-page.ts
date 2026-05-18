b4f865b937386b1c4c2515140611b415aa0141a3
SlavikusVOG <slavaolish@gmail.com>
2026-05-18 17:51:22 +0300

    feat: define cart item types

--- src/app/features/cart/pages/cart-page/cart-page.ts
+++ src/app/features/cart/pages/cart-page/cart-page.ts
@@ -1,9 +1,46 @@
 import { Component } from '@angular/core';
 
+type TypedMoney = {
+  type: string;
+  currencyCode: string;
+  centAmount: number;
+  fractionDigits: number;
+};
+
+type Price = {
+  id: string;
+  key: string;
+  value: TypedMoney;
+};
+
+type LineItem = {
+  id: string;
+  key: string;
+  productId: string;
+  productKey: string;
+  name: string;
+  productType: string;
+  price: Price;
+};
+
+type CartItem = {
+  id: string;
+  version: number;
+  key: string;
+  customerId: string;
+  customerEmail: string;
+  store: string;
+  lineItems: LineItem[];
+};
+
 @Component({
   selector: 'xas-cart-page',
   imports: [],
   templateUrl: './cart-page.html',
   styleUrl: './cart-page.scss',
 })
-export class CartPage {}
+export class CartPage {
+  fetchCartItems(): CartItem[] {
+    return [];
+  }
+}
