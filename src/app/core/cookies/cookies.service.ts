62a56bad0909390905dd25e99feed3a695072c08
SlavikusVOG <slavaolish@gmail.com>
2026-06-03 02:46:25 +0300

    feat: add cookies service

---
+++ src/app/core/cookies/cookies.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CookiesService {
+  // public addCookie(name: string, value: string, expires: Date) {
+  //   cookieStore.set(name, value, expires);
+  // }
+}
