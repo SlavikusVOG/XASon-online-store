32dbcb1183272253642a0056ce22fe72c51a0656
SlavikusVOG <slavaolish@gmail.com>
2026-06-11 13:36:36 +0300

    refactor: use signals for access and refresh tokens

--- src/app/core/auth/index.ts
+++ src/app/core/auth/index.ts
@@ -1,4 +1,3 @@
 export * from './services/anonymous-session.service';
-export * from './services/app-credentials.service';
 export * from './services/auth.service';
 export * from './services/customer-session.service';
