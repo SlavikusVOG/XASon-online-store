6e2c0d5ebec0aa8d585159f5642e947d83ba9e33
SlavikusVOG <slavaolish@gmail.com>
2026-06-22 17:54:17 +0300

    feat: add back to home link

--- src/app/features/authentication/pages/login-page/login-page.ts
+++ src/app/features/authentication/pages/login-page/login-page.ts
@@ -1,15 +1,18 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { AuthService } from '@core/auth';
+import { PAGES } from '@core/router/pages.const';
 import { LoginForm } from '@features/authentication/components';
 import { LoginCredentials } from '@models/features/authentication';
 
 @Component({
   selector: 'xas-login-page',
-  imports: [LoginForm],
+  imports: [LoginForm, RouterLink],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
+  protected readonly PAGES = PAGES;
   private readonly authService = inject(AuthService);
   onLogin({ email, password }: LoginCredentials): void {
     if (!email || !password) {
