e205ea817365664780d494ab5bf24fdfe8678498
SlavikusVOG <slavaolish@gmail.com>
2026-06-28 18:08:30 +0300

    feat: add apiUrl to environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  apiUrl: import.meta.env['NG_APP_API_URL'],
   commercetools: {
     projectKey: import.meta.env['NG_APP_PROJECT_KEY'],
     clientId: import.meta.env['NG_APP_CLIENT_ID'],
