a5df69ee1d51c6c799bbe940355b0d5a70812b5d
SlavikusVOG <slavaolish@gmail.com>
2026-06-29 17:29:04 +0300

    feat: update products types

--- src/app/models/features/catalog/product.types.ts
+++ src/app/models/features/catalog/product.types.ts
@@ -1,8 +1,54 @@
-// TODO моковый тип, актуализировать когда завяжемся на данные с коммерстулзов
+type ProductCatalogData = {
+  published?: boolean;
+  current: ProductData;
+  staged?: ProductData;
+};
+
+type ProductData = {
+  name: string;
+  description: string;
+  variants: ProductVariant;
+};
+
+type ProductVariant = {
+  id: number;
+  key: string;
+  images: Image[];
+};
+
+type Image = {
+  url: string;
+  dimensions?: ImageDimensions;
+};
+
+type ImageDimensions = {
+  w: number;
+  h: number;
+};
+
 export type Product = {
   id: string;
   name: string;
   image: string;
   description: string;
   isInCart: boolean;
+};
+
+export type ProductPagedQueryResponse = {
+  limit: number;
+  offset: number;
+  count: number;
+  total: number;
+  results: Product[];
+};
+
+export type CatalogProductsGetQueries = {
+  limit: number;
+  offset: number;
+};
+
+export type ProductsDto = {
+  id: string;
+  key: string;
+  masterData: ProductCatalogData;
 };
