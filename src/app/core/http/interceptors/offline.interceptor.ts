642b4472bc4718a26b60c40588b211a50d94fcc4
xevoider <xevoider@gmail.com>
2026-07-04 00:20:25 +0300

    feat: add toast service

--- src/app/core/http/interceptors/offline.interceptor.ts
+++ src/app/core/http/interceptors/offline.interceptor.ts
@@ -1,14 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { NetworkService } from '@core/http/services/network.service';
+import { ToastService } from '@shared/services';
 import { EMPTY } from 'rxjs';
 
 export const offlineInterceptor: HttpInterceptorFn = (req, next) => {
+  const toastService = inject(ToastService);
   const networkService = inject(NetworkService);
 
   if (networkService.isOffline()) {
-    // todo тут вместо алерта сделать тост
-    alert('You are offline.');
+    toastService.showErrorToast('You are offline. Connect to Internet to proceed');
     return EMPTY;
   }
 
---
+++ src/app/shared/services/toast/toast.service.ts
@@ -0,0 +1,40 @@
+import { inject, Service } from '@angular/core';
+import { TuiNotificationService } from '@taiga-ui/core';
+import { Subject, takeUntil } from 'rxjs';
+
+type ToastAppearance = 'info' | 'warning' | 'negative' | 'positive' | 'neutral';
+
+@Service()
+export class ToastService {
+  private readonly notificationService = inject(TuiNotificationService);
+
+  private readonly _closeAllToasts = new Subject<void>();
+
+  public showInfoToast(text: string): void {
+    return this.showToast(text, 'info');
+  }
+
+  public showErrorToast(text: string): void {
+    return this.showToast(text, 'negative');
+  }
+
+  public showSuccessToast(text: string): void {
+    return this.showToast(text, 'positive');
+  }
+
+  public closeToasts(): void {
+    this._closeAllToasts.next();
+  }
+
+  private showToast(text: string, appearance: ToastAppearance): void {
+    this.notificationService
+      .open(text, {
+        appearance,
+        autoClose: 3_000,
+        block: 'start',
+        inline: 'center',
+      })
+      .pipe(takeUntil(this._closeAllToasts))
+      .subscribe();
+  }
+}
