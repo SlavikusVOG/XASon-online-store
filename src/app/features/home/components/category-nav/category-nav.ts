1cc8e30503f4abd070996ae22be31fd0bd66231c
Anna Galkevich <emikot321@gmail.com>
2026-07-06 00:47:07 +0300

    feat: add styles for category nav

--- src/app/features/home/components/category-nav/category-nav.ts
+++ src/app/features/home/components/category-nav/category-nav.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TuiAppearance, TuiTitle } from '@taiga-ui/core';
+import { TuiAutoColorPipe, TuiInitialsPipe } from '@taiga-ui/kit';
 import { PAGES } from '@core/router/pages.const';
 import { Category } from '@models/features/home';
 
 @Component({
   selector: 'xas-category-nav',
-  imports: [RouterLink],
+  imports: [RouterLink, TuiAppearance, TuiTitle, TuiAutoColorPipe, TuiInitialsPipe],
   templateUrl: './category-nav.html',
   styleUrl: './category-nav.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
