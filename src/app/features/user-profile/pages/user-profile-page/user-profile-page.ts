5c779a476f445eb92e425c6c54ac0973cabc8a4d
SlavikusVOG <slavaolish@gmail.com>
2026-07-06 18:29:01 +0300

    feat: implement profile page with personal information

--- src/app/features/user-profile/pages/user-profile-page/user-profile-page.ts
+++ src/app/features/user-profile/pages/user-profile-page/user-profile-page.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { PersonalInformation } from '@features/user-profile/components';
 import { User } from '@models/features/user-profile';
+import { ApiService } from '@core/http';
+import { DATA_LOAD_STATUSES, DataLoadStatus } from '@shared/const';
 
 @Component({
   selector: 'xas-user-profile-page',
@@ -8,16 +10,34 @@ import { User } from '@models/features/user-profile';
   templateUrl: './user-profile-page.html',
   styleUrl: './user-profile-page.scss',
 })
-export class UserProfilePage {
-  user: User = {
-    id: '1',
-    email: 'test@test.com',
-    firstName: 'John',
-    lastName: 'Doe',
-    dateOfBirth: new Date(),
-    street: '123 Main St',
-    city: 'Anytown',
-    postalCode: '12345',
-    country: 'USA',
-  };
+export class UserProfilePage implements OnInit {
+  private readonly apiService = inject(ApiService);
+  protected readonly loadStatus = signal<DataLoadStatus>(DATA_LOAD_STATUSES.INIT);
+  protected readonly user = signal<User | null>(null);
+  // userMock: ProcessedUser = {
+  //   id: '1',
+  //   email: 'test@test.com',
+  //   firstName: 'John',
+  //   lastName: 'Doe',
+  //   dateOfBirth: new Date(),
+  //   street: '123 Main St',
+  //   city: 'Anytown',
+  //   postalCode: '12345',
+  //   country: 'USA',
+  // };
+
+  ngOnInit(): void {
+    this.loadStatus.set(DATA_LOAD_STATUSES.LOADING);
+    this.apiService
+      .getMe({
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .subscribe({
+        next: (user) => {
+          this.user.set(user);
+        },
+      });
+  }
 }
