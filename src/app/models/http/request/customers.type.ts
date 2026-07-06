713141160473893de31fd2f0087dcd9cb2bbd692
SlavikusVOG <slavaolish@gmail.com>
2026-07-06 10:31:22 +0300

    fix: update customer types

--- src/app/models/http/request/customers.type.ts
+++ src/app/models/http/request/customers.type.ts
@@ -1,10 +1,10 @@
 export type CustomersPostResponse = {
-  addresses: [];
-  email: string;
-  firstName: string;
-  id: string;
-  lastName: string;
-  password: string;
+  access_token: string;
+  expires_in: number;
+  token_type: string;
+  scope: string;
+  refresh_token: string;
+  customer: Customer;
 };
 
 export type CustomersPostBody = {
@@ -12,4 +12,32 @@ export type CustomersPostBody = {
   password: string;
   firstName: string;
   lastName: string;
+};
+
+type Customer = {
+  id: string;
+  version: number;
+  versionModifiedAt: string;
+  lastMessageSequenceNumber: number;
+  createdAt: string;
+  lastModifiedAt: string;
+  lastModifiedBy: {
+    clientId: string;
+    isPlatformClient: boolean;
+  };
+  createdBy: {
+    clientId: string;
+    isPlatformClient: boolean;
+  };
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  addresses: [];
+  shippingAddressIds: string[];
+  billingAddressIds: string[];
+  isEmailVerified: boolean;
+  customerGroupAssignments: string[];
+  stores: string[];
+  authenticationMode: string;
 };
