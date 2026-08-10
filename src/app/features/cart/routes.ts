c12e1c0936dd3ce062d745495364ee2bacaff526
SlavikusVOG <slavaolish@gmail.com>
2026-08-10 20:08:37 +0300

    feat: add cart store provider to cart route

--- src/app/features/cart/routes.ts
+++ src/app/features/cart/routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { CartStore } from '@features/cart/store';
 
 export const cartRoutes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/cart-page/cart-page').then((c) => c.CartPage),
+    providers: [CartStore],
   },
 ];
