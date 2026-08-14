3804864f0233294cc27818d4f7057b2aa7e8bc5c
SlavikusVOG <slavaolish@gmail.com>
2026-08-14 11:39:01 +0300

    fix: fix anonymous session handling

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -1,18 +1,12 @@
 import { inject, Injectable } from '@angular/core';
 import { LoginCredentials, RegisterCredentials } from '@models/features/authentication';
 import { CustomerSessionService } from './customer-session.service';
-import { AnonymousSessionService } from './anonymous-session.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private readonly customerSessionService = inject(CustomerSessionService);
-  private readonly anonymousSessionService = inject(AnonymousSessionService);
-
-  constructor() {
-    this.ensureAnonymousToken();
-  }
 
   isAuthenticated() {
     return this.customerSessionService.getAccessToken() !== null;
@@ -25,11 +19,5 @@ export class AuthService {
   register(credentials: RegisterCredentials) {
     const response = this.customerSessionService.register(credentials);
     return response;
-  }
-
-  private ensureAnonymousToken() {
-    if (!this.isAuthenticated()) {
-      this.anonymousSessionService.ensureAccessToken();
-    }
   }
 }
--- src/app/core/auth/services/customer-session.service.ts
+++ src/app/core/auth/services/customer-session.service.ts
@@ -4,7 +4,6 @@ import { ApiService } from '@core/http';
 import { LocalStorage } from '@core/local-storage';
 import { environment } from '@environments/environment';
 import { LoginCredentials, RegisterCredentials } from '@models/features/authentication';
-import { AnonymousSessionService } from './anonymous-session.service';
 import { map, Observable, of, switchMap, tap } from 'rxjs';
 import { LoginPostResponse } from '@models/http';
 import {
@@ -20,7 +19,6 @@ import { Address, PersonalInfoUpdate, ProcessedUser } from '@models/features/use
 export class CustomerSessionService {
   private readonly localStorageService = inject(LocalStorage);
   private readonly apiService = inject(ApiService);
-  private readonly anonymousSessionService = inject(AnonymousSessionService);
   private accessToken = signal<string | null>(
     this.localStorageService.getValue<string>(environment.LOCAL_STORAGE_KEYS.accessToken),
   );
@@ -33,24 +31,30 @@ export class CustomerSessionService {
 
   constructor() {
     effect(() => {
-      this.localStorageService.setValue(
-        environment.LOCAL_STORAGE_KEYS.accessToken,
-        this.accessToken(),
-      );
+      const token = this.accessToken();
+      if (token) {
+        this.localStorageService.setValue(environment.LOCAL_STORAGE_KEYS.accessToken, token);
+      } else {
+        this.localStorageService.removeValue(environment.LOCAL_STORAGE_KEYS.accessToken);
+      }
     });
 
     effect(() => {
-      this.localStorageService.setValue(
-        environment.LOCAL_STORAGE_KEYS.refreshToken,
-        this.refreshToken(),
-      );
+      const token = this.refreshToken();
+      if (token) {
+        this.localStorageService.setValue(environment.LOCAL_STORAGE_KEYS.refreshToken, token);
+      } else {
+        this.localStorageService.removeValue(environment.LOCAL_STORAGE_KEYS.refreshToken);
+      }
     });
 
     effect(() => {
-      this.localStorageService.setValue<ProcessedUser | null>(
-        environment.LOCAL_STORAGE_KEYS.user,
-        this.user(),
-      );
+      const user = this.user();
+      if (user) {
+        this.localStorageService.setValue<ProcessedUser>(environment.LOCAL_STORAGE_KEYS.user, user);
+      } else {
+        this.localStorageService.removeValue(environment.LOCAL_STORAGE_KEYS.user);
+      }
     });
   }
 
